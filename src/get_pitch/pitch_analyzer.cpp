4ac1a8e52464e76aa307b4216fde384f30df34a7
Albert Caus Solé <albert.caus@upc.edu>
2025-04-11 09:59:10 +0200

    Classe 11/04

--- src/get_pitch/pitch_analyzer.cpp
+++ src/get_pitch/pitch_analyzer.cpp
@@ -8,10 +8,11 @@ using namespace std;
 
 /// Name space of UPC
 namespace upc {
-  void PitchAnalyzer::autocorrelation(const vector<float> &x, vector<float> &r) const {
+  void PitchAnalyzer::autocorrelation(const vector<float> &x, vector<float> &r) const {  
 
     for (unsigned int l = 0; l < r.size(); ++l) {
   		/// \TODO Compute the autocorrelation r[l]
+      /// \FET "Hem fet l'autocorrelació"
     }
 
     if (r[0] == 0.0F) //to avoid log() and divide zero 
